a4895f62ef0b42da3e51c29f0893408d7483cf05
MrsHines <34042849+MrsHines@users.noreply.github.com>
2019-02-13 12:02:12 -0600

    Starter Code

---
+++ oop.py
@@ -0,0 +1,20 @@
+# create at least one class for each of the three programs.  
+# The main definition should be the only definition located OUTSIDE of a class
+# Create the objects and run the functions from within the main
+
+def main():
+
+
+class ThreeButtonMonte:
+
+
+
+class CButton:
+
+
+
+class Face: 
+
+
+
+main()
